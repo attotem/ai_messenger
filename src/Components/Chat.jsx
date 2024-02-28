8036073cc05479dcfb3df48ff73026731754ca35
attotem <vshinkarenko21@gmail.com>
2024-02-28 13:00:42 +0100

    working version

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ChatMessage from './ChatMessage';
 import MessageInput from './MessageInput';
+import { ThreeDots } from 'react-loader-spinner'; // Make sure this line is added
 import './css.css';
 
 function Chat() {
     const [messages, setMessages] = useState([]);
     const [thread_id, setThread_id] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
     const messagesEndRef = useRef(null);
 
     const handleSendMessage = (newMessage) => {
         setMessages(prevMessages => [...prevMessages, { text: newMessage, isUserMessage: true }]);
-        console.log(newMessage)
+        console.log(newMessage);
+        setIsLoading(true);
 
         fetch("https://webhoooks-dmitrykarpov.pythonanywhere.com/add_message", {
             method: "POST",
@@ -26,12 +30,16 @@ function Chat() {
         })
             .then(response => response.json())
             .then(data => {
-                console.log(data)
-                setMessages(prevMessages => [...prevMessages, { text: data.answer, isUserMessage: false }]);
-
+                console.log(data);
+                setIsLoading(false);
+                setMessages(prevMessages => [...prevMessages, { text: data.res, isUserMessage: false }]);
             })
-            .catch(error => { console.error("Error fetching data:", error); })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+                setIsLoading(false);
+            });
     };
+
 
     useEffect(() => {
         fetch("https://webhoooks-dmitrykarpov.pythonanywhere.com/create_thread", {
@@ -64,6 +72,17 @@ function Chat() {
                 {messages.map((message, index) => (
                     <ChatMessage key={index} message={message.text} isUserMessage={message.isUserMessage} />
                 ))}
+                {isLoading &&
+                    < ThreeDots
+                        visible={true}
+                        height="40"
+                        width="40"
+                        color="#c6c6c6"
+                        radius="8"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                    />}
                 <div ref={messagesEndRef} />
             </div>
             <MessageInput onSendMessage={handleSendMessage} />
